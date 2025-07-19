9abfea4d13c980041a24fc7894bad62ca2a612ee
Stefan Gojan <vollekannehoschi@gmail.com>
2025-07-19 17:34:03 +0200

    special run for current model after broader run earlier

--- server/constants.py
+++ server/constants.py
@@ -8,11 +8,11 @@ DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
 N_VALIDATION = EXAMPLES_COUNT_DEBUG if DEBUG else EXAMPLES_COUNT_REAL
 
 # Wertebereich für Alpha im Grid Search
-ALPHA_RANGE = [round(x, 2) for x in list(__import__('numpy').linspace(0.0, 1, num=5))]
+ALPHA_RANGE = [round(x, 2) for x in list(__import__('numpy').linspace(0.3, 0.6, num=7))]
 if DEBUG:
     ALPHA_RANGE = [ALPHA_RANGE[0]]
 
 # Wertebereich für Beta im Grid Search
-beta_range = [round(x, 2) for x in list(__import__('numpy').linspace(-2.0, 2.0, num=10))]
+beta_range = [round(x, 2) for x in list(__import__('numpy').linspace(-1.5, -0.9, num=7))]
 if DEBUG:
     beta_range = [beta_range[0]]
