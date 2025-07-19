56a34f62e0e701634f45d2c6eb1a145b187e8d1a
Stefan Gojan <vollekannehoschi@gmail.com>
2025-07-19 09:56:09 +0200

    add debug mode again

--- server/constants.py
+++ server/constants.py
@@ -1,9 +1,16 @@
 # Konstanten für das Decoder-Tuning und andere Server-Module
 
-N_VALIDATION = 4 # 370
+import os
+
+DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
+N_VALIDATION = 50 if DEBUG else 370
 
 # Wertebereich für Alpha im Grid Search
-ALPHA_RANGE = [0.2] # [round(x, 2) for x in list(__import__('numpy').arange(0.0, 1, 0.1))] 
+ALPHA_RANGE = [round(x, 2) for x in list(__import__('numpy').arange(0.0, 1, 0.1))]
+if DEBUG:
+    ALPHA_RANGE = [ALPHA_RANGE[0]]
 
 # Wertebereich für Beta im Grid Search
-beta_range = [-1.0] # [round(x, 2) for x in list(__import__('numpy').arange(-2.0, 2.1, 0.25))]
+beta_range = [round(x, 2) for x in list(__import__('numpy').arange(-2.0, 2.1, 0.25))]
+if DEBUG:
+    beta_range = [beta_range[0]]
